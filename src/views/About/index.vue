98d13e26060dc73d8a5d25c99ae2cc4cea0a603c
tharwat74 <mohamedtharout190@gmail.com>
2020-04-25 21:22:40 +0200

    i'm playing with vue.js

---
+++ src/views/About/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="styles.wrapper">
+    <div :class="styles.item" v-for="(item, x) in title" :key="{ x }">
+      <h1>{{ item.name }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import styles from "./styles.module.sass";
+export default {
+  data: () => ({
+    title: [
+      { name: "Hello" },
+      { name: "form" },
+      { name: "about" },
+      { name: "page" },
+    ],
+    styles,
+  }),
+};
+</script>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="styles.wrapper">
+    <HelloWorld msg="Hello from Home page" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import styles from "./styles.module.sass";
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    styles,
+  }),
+};
+</script>
